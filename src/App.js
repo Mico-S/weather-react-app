e5b0163281e3d82154fe74989c762996ec7ed47d
Micah Cook <mico8947@colorado.edu>
2022-03-01 12:19:04 -0700

    Minor addition

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react'
 import axios from 'axios'
 
 function App() {
+  // OpenWeatherMap API request URL
+  // Parameters: city, API key
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=denver&appid=c02bacfef72fca3b23d3c402e65dc9f7`
+
   return (
     <div className="App">
       
